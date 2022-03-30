7a579dd3c3982c99493a90269035ef5897a0932e
Henrique Santos <rickalmeida720@gmail.com>
2022-03-30 08:31:41 -0300

    Button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
+import NavBar from 'components/NavBar';
 import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div >
+    <>
+   <NavBar />
+    <div > 
     <h2>"Nada aqui por enquanto"</h2>
     </div>
+    </>
   );
 }
 
---
+++ frontend/src/components/Button/index.tsx
@@ -0,0 +1,7 @@
+import './styles.css'
+
+export function Button(){
+    return (
+<button className='cl-btn'>Sing Up</button>
+    );
+}

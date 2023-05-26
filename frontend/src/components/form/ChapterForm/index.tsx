f3ac85d310e9b0dbb9db0f3503c5d74cde7c60eb
Henrique Santos <rickalmeida720@gmail.com>
2023-05-26 10:06:14 -0300

    component: ChapterEditForm

--- frontend/src/components/form/ChapterForm/index.tsx
+++ frontend/src/components/form/ChapterForm/index.tsx
@@ -1,6 +1,8 @@
 import axios, { AxiosRequestConfig } from "axios";
-import React, {  } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Chapter } from "types/chapter";
+import { Props } from "types/quiz";
 import { BASE_URL } from "utils/requests";
 
 export function ChapterAddForm() {
@@ -18,7 +20,7 @@ export function ChapterAddForm() {
             data: {
                 title: title,
                 description: description,
-                image: image,
+                image: image
             }
         }
         axios(config).then((response) => {
@@ -57,6 +59,66 @@ export function ChapterAddForm() {
     );
 }
 
+export function ChapterEditForm({ id: chapterId }: Props) {
+    const navigate = useNavigate();
 
+    const [chapter, setChapter] = useState<Chapter>();
+    useEffect(() => {
+        axios.get(`${BASE_URL}/chapter/${chapterId}`)
+            .then((response) => {
+                setChapter(response.data);
+            })
+    }, [chapterId]);
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        const title = (event.target as any).title.value;
+        const description = (event.target as any).description.value;
+        const image = (event.target as any).image.value;
+
+        const config: AxiosRequestConfig = {
+            method: 'PUT',
+            baseURL: BASE_URL,
+            url: `/chapter/edit`,
+            data: {
+                chapterId: chapterId,
+                title: title,
+                description: description,
+                image: image
+            }
+        }
+        axios(config).then((response) => {
+            navigate(`/`);
+        })
+    }
+
+    return (
+        <>
+            <form onSubmit={handleSubmit} className="form-container">
+                <div className="modal-header">
+                    <div className="modal-title" id="itemLabel">Editar capítulo</div>
+                    <button type="submit" data-bs-dismiss="modal" className="btn">Editar</button>
+                    <button type="button" className="close" data-bs-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true"><i className="fa fa-times" /></span>
+                    </button>
+                </div>
+
+                <div className="form-card">
+                    <div className="form-group">
+                        <label htmlFor="title">Título: </label>
+                        <input id="title" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="id">Descrição: </label>
+                        <input id="description" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="image">Imagem: </label>
+                        <input id="image" />
+                    </div>
+                </div>
+            </form>
+        </>
+    );
+}
 
 
